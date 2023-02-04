1a69e20edb1b95614929192372d931007ec7bf98
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2023-02-04 22:11:26 +0530

    StringBuilder example

---
+++ stringclass/src/stringclass/StringBuilder1.java
@@ -0,0 +1,12 @@
+package stringclass;
+
+public class StringBuilder1 {
+
+	public static void main(String[] args) {
+
+		StringBuffer sb = new StringBuffer("Durga");
+		sb.append("Software");
+		System.out.println(sb);
+	}
+
+}
